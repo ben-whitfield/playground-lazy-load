cf3118a3f62f0750f5b31e8b95573e675fca9233
benwdev <bwhitfield82@gmail.com>
2021-05-05 23:06:08 +0100

    basic functionality

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hi")
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+const items = document.getElementsByClassName('item')
+
+document.addEventListener('scroll', function () {
+  Array.from(items).forEach(item => {
+    if(isInViewport(item)) {
+      item.classList.add('shown');
+    } else {
+      item.classList.remove('shown')
+    }
+  });
+}, {
+  passive: true
+});
