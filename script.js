e8855448a9b2c947d4ee1f895a8e77fbfec5c795
benwdev <bwhitfield82@gmail.com>
2021-05-06 08:20:58 +0100

    change to out of viewport logic and css transition

--- script.js
+++ script.js
@@ -1,23 +1,44 @@
-function isInViewport(element) {
+function isOffTop(rect) {
+  return (rect.bottom + 100) < 0
+}
+
+function isOffBottom(rect) {
+  return (rect.top + 100) > (window.innerHeight || document.documentElement.clientHeight)
+}
+
+function isOffRight(rect) {
+  return (rect.left + 100) > (window.innerWidth || document.documentElement.clientWidth)
+}
+
+function isOffLeft(rect) {
+  return (rect.right + 100) < 0
+}
+
+function isOutOfViewport(element) {
   const rect = element.getBoundingClientRect();
   return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      isOffTop(rect) || isOffLeft(rect) || isOffBottom(rect) || isOffLeft(rect)
   );
+}
+
+function updateVisibility(items) {
+  Array.from(items).forEach(item => {
+    if(isOutOfViewport(item)) {
+      item.classList.remove('shown');
+    } else {
+      item.classList.add('shown')
+    }
+  });
 }
 
 const items = document.getElementsByClassName('item')
 
 document.addEventListener('scroll', function () {
-  Array.from(items).forEach(item => {
-    if(isInViewport(item)) {
-      item.classList.add('shown');
-    } else {
-      item.classList.remove('shown')
-    }
-  });
+  updateVisibility(items)
 }, {
   passive: true
 });
+
+window.addEventListener('load', (event) => {
+  updateVisibility(items)
+})
